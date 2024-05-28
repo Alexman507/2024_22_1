1fd5ff892a7b5e88439f8fd789208c648c47b5eb
NeroPC <Nerocross507@gmail.com>
2024-05-29 00:37:49 +0300

    OrderCreateView

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from main.apps import MainConfig
-from main.views import ProductListView, ProductDetailView, pagination, ContactListView
+from main.views import ProductListView, ProductDetailView, ContactListView, OrderCreateView
 
 app_name = MainConfig.name
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('products/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
-    path('list_products/<int:per_page>/<int:page>/', pagination, name='pagination'),
     path('contacts/', ContactListView.as_view(), name='contacts'),
+    path('create/', OrderCreateView.as_view(), name='create_order'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,29 +1,13 @@
-from datetime import datetime
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
-
-from main.models import Product, Category, Contact, Order
+from main.models import Product, Contact, Order
 
 
 # Create your views here.
 
 class ProductListView(ListView):
     model = Product
-
-
-# def home(request):
-#     products_list = Product.objects.all()
-#     category_list = Category.objects.all()
-#     # Product.objects.all().delete()
-#     # Category.objects.all().delete()
-#     #
-#     context = {
-#         'object_list': products_list,
-#         'title': 'Каталог'
-#     }
-#
-#     return render(request, 'main/product_list.html', context)
 
 
 class ProductDetailView(DetailView):
@@ -35,69 +19,16 @@ class ProductDetailView(DetailView):
         return context
 
 
-# def product_pk(request, pk, page=None, per_page=None):
-#     obj = get_object_or_404(Product, pk=pk)
-#     context = {
-#         "object": obj,
-#         "pagination": bool(per_page),
-#         "per_page": per_page,
-#         "page": page
-#     }
-#
-#     return render(request, 'main/product_detail.html', context)
-
-
-def pagination(request, page, per_page):
-    len_product = len(Product.objects.all())
-    if len_product % per_page != 0:
-        page_count = [x + 1 for x in range((len_product // per_page) + 1)]
-    else:
-        page_count = [x + 1 for x in range((len_product // per_page))]
-
-    product_list = Product.objects.all()[per_page * (page - 1): per_page * page]
-
-    context = {
-        "product_list": product_list,
-        "page": page,
-        "per_page": per_page,
-        "page_count": page_count
-    }
-
-    return render(request, 'main/per_page.html', context)
-
-
 class ContactListView(ListView):
     model = Contact
 
-# def contacts(request):
-#     number = len(Contact.objects.all())
-#     if number > 5:
-#         contacts_list = Contact.objects.all()[number - 5: number + 1]
-#     else:
-#         contacts_list = Contact.objects.all()
-#
-#     context = {
-#         'object_list': contacts_list,
-#         'title': 'Контакты'
-#     }
-#
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         phone = request.POST.get('phone')
-#         message = request.POST.get('message')
-#
-#         info = {'created_at': (datetime.now()).strftime('%Y-%m-%dT%H:%M'),
-#                 'name': name, 'phone': phone, 'message': message
-#                 }
-#
-#         Order.objects.create(**info)
-#
-#     return render(request, 'main/contact_list.html', context)
 
-# def index(request):
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         email = request.POST.get('email')
-#         message = request.POST.get('message')
-#         print(f'{name} ({email}): {message}')
-#     return render(request, 'main/index.html')
+class OrderCreateView(CreateView):
+    model = Order  # Модель
+    fields = ('name', 'phone', 'message', 'created_at')  # Поля для заполнения при создании
+    success_url = reverse_lazy('main:product_list')  # Адрес для перенаправления после успешного создания
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        response = super().form_valid(form)
+        return response
