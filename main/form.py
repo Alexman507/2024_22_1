c0b4c65906eb8e23f93b9f2c1d75aa0d7611b276
SRE <Nerocross507@gmail.com>
2024-06-10 09:35:40 +0300

    work with classes ClassForm

--- main/form.py
+++ main/form.py
@@ -1,9 +1,24 @@
 from django import forms
 
-from main.models import Product
+from main.models import Product, Contact
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    def clean(self):
+        pass
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = '__all__'
+
+
+# class VersionForm(forms.ModelForm):
+#     class Meta:
+#         model = Version
+#         fields = '__all__'
