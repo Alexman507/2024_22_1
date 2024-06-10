585a97ee51a883782bd518a4f947502d0e3fd580
SRE <Nerocross507@gmail.com>
2024-06-10 10:00:37 +0300

    added StyleFormMixin

--- main/form.py
+++ main/form.py
@@ -1,9 +1,20 @@
 from django import forms
+from django.forms import BooleanField
 
 from main.models import Product, Contact, Version
 
 
-class ProductForm(forms.ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
@@ -22,13 +33,13 @@ class ProductForm(forms.ModelForm):
             return self.cleaned_data
 
 
-class ContactForm(forms.ModelForm):
+class ContactForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Contact
         fields = '__all__'
 
 
-class VersionForm(forms.ModelForm):
+class VersionForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Version
         fields = '__all__'
